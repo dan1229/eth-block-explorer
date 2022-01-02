09f7e54b89a26d9f56548b82642e9bfa716ea1d1
Daniel Nazarian <danielnazarian@outlook.com>
2022-01-01 22:19:44 -0500

    more refactoring

--- helpers.py
+++ helpers.py
@@ -3,7 +3,7 @@ from .consts import *
 
 # ==========================
 #
-# HELPERS
+# CHOICE HELPERS
 #
 def valid_menu_choice(choice):
     try:
@@ -26,3 +26,36 @@ def get_choice_by_num(num):
         if int(menu_choice[1]) == num:
             return menu_choice
     return None
+
+
+# ==========================
+#
+# PRINT FUNCTIONS
+#
+def print_line_break():
+    print("================================")
+    
+def print_menu():
+    print_line_break()
+    print("=")
+    print("= Search by:")
+    for menu_choice in LIST_MENU_CHOICES:
+        print("= " + str(menu_choice[1]) + ".\t" + str(menu_choice[0]))
+    print("= ")
+    print_line_break()
+    print("")
+    
+# 
+# get_menu_choice
+# @[RETURN]
+# int
+#   > 0     - menu choice selected
+#   < 0     - invalid menu choice selected
+#
+def get_menu_choice():
+    print_menu()    
+    choice = input("Choice >> ")
+    if not valid_menu_choice(choice):
+        print("\nERR: invalid choice. Please try again.\n")
+        return -1
+    return int(choice)
